9bf09a4312156fb51ea0942cbc76572395fc380b
Temirlan <69697147+JustTrott@users.noreply.github.com>
2024-12-06 23:01:38 +0400

    remove cohere remains

--- app/config.py
+++ app/config.py
@@ -7,7 +7,6 @@ load_dotenv()
 class Config:
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
     API_V1_STR = "/api/v1"
-    COHERE_API_KEY = os.getenv("COHERE_API_KEY")
     OPENFDA_API_KEY = os.getenv("OPENFDA_API_KEY")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
